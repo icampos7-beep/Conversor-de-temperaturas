98ce5fdbb196472c4322695ab7ec265ce0998c41
icampos7-beep <icampos7@ucol.mx>
2026-09-21 12:22:12 -0600

    Agregar archivos de proyecto.

---
+++ Conversor de temperaturas/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Conversor_de_temperaturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCen = new System.Windows.Forms.TextBox();
+            this.txtFah = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grados Centigrados:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Fahrenheit:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCen
+            // 
+            this.txtCen.Location = new System.Drawing.Point(220, 42);
+            this.txtCen.Name = "txtCen";
+            this.txtCen.Size = new System.Drawing.Size(100, 20);
+            this.txtCen.TabIndex = 3;
+            this.txtCen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCen_KeyPress);
+            // 
+            // txtFah
+            // 
+            this.txtFah.Location = new System.Drawing.Point(208, 107);
+            this.txtFah.Name = "txtFah";
+            this.txtFah.Size = new System.Drawing.Size(114, 20);
+            this.txtFah.TabIndex = 4;
+            this.txtFah.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFah_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 245);
+            this.Controls.Add(this.txtFah);
+            this.Controls.Add(this.txtCen);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCen;
+        private System.Windows.Forms.TextBox txtFah;
+    }
+}
+
---
+++ Conversor de temperaturas/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor_de_temperaturas
+{
+    public partial class Form1 : Form
+    {
+        //Agregamos un objeto TextBox
+        private TextBox objTextBox = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Conversion()
+        {
+            try
+            {
+                double grados;
+                //Si se escribe en la caja e texto grdos centigrados...
+                if(objTextBox == txtCen)
+                {
+                    grados = Convert.ToDouble(txtCen.Text);
+                    txtFah.Text = ((grados * 9/5) + 32).ToString();
+                }
+                if (objTextBox == txtFah)
+                {
+                    grados = (Convert.ToDouble(txtFah.Text)-32.0) * 5.0/9.0;
+                    txtCen.Text = string.Format("{0:F2}", grados);
+                }
+            }catch(FormatException)
+            {
+                txtCen.Text = "0.00";
+                txtFah.Text = "32.00";
+            }
+        }
+
+        private void txtCen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objTextBox = (TextBox)sender;
+            if(e.KeyChar == Convert.ToChar(13))
+
+            {
+                //Enter
+                e.Handled = true;
+                Conversion();
+            }
+        }
+
+        private void txtFah_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objTextBox = (TextBox)sender;
+            if (e.KeyChar == Convert.ToChar(13))
+
+            {
+                //Enter
+                e.Handled = true;
+                Conversion();
+            }
+        }
+    }
+}
